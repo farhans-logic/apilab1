3a2dd87d668673cb58c47d41d75694b7bba85175
Farhan <142213240+farhans-logic@users.noreply.github.com>
2023-09-25 04:33:35 +0100

    initial commit

---
+++ src/main/java/com/example/apilab1/ApiLab1Application.java
@@ -0,0 +1,14 @@
+package com.example.apilab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLab1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiLab1Application.class, args);
+    }
+
+}
